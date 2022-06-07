d25d657cd1f51b47629eb2761d86b9bc5e24fc6b
Alessa <alosherto@gmail.com>
2022-06-07 18:08:00 -0400

    2ndcommit

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Alessa De Leon Chavez on 2022-06-07.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
